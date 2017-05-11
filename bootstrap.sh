c5ecdef7498a8b42aa3cc6f9ffc276faa2cfa849
shvetsanton <ashvets23@gmail.com>
2017-05-10 22:06:29 -0700

    Add files via upload

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+sudo apt-get -y update 
+sudo apt install -y python3-pip
+sudo python3 -m pip install -y pip requests os yaml boto awscli -U
+#aws configure
---
+++ run.sh
@@ -0,0 +1,1 @@
+python3 /home/ubuntu/stream_dark_sky.py
